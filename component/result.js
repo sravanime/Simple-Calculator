b2b70592cd1cb589a6f6dbb08d504eb812262274
sravanime <129075197+sravanime@users.noreply.github.com>
2023-03-27 20:04:58 +0530

    Add files via upload

---
+++ component/result.js
@@ -0,0 +1,12 @@
+import React,{component} from 'react'
+class result extends component {
+    render(){
+        let result = this.props;
+        return (
+              <div className='result'>
+                <p>(result)</p>
+              </div>
+        )
+    }
+}
+export default result
